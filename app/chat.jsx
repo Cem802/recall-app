febb37e8f342ff0a46226f1af6cada7f838ca565
Cem802 <ckirciltepeli@gmail.com>
2024-04-27 20:24:05 +0200

    Add LinearGradient component to chat and index files

--- app/chat.jsx
+++ app/chat.jsx
@@ -4,10 +4,15 @@ import { SafeAreaView } from 'react-native-safe-area-context'
 import Input from '../components/Input'
 import KeyboardUsingContainer from '../components/KeyboardUsingContainer'
 import Header from '../components/Header'
+import { LinearGradient } from 'expo-linear-gradient'
 
 const chat = () => {
   return (
     <SafeAreaView className="bg-primary h-full">
+      <LinearGradient
+          colors={['#0E0C1D', '#07060E']}
+          className="absolute h-[100vh] left-0 right-0 top-0"
+      />
         <KeyboardUsingContainer>
             <View className="h-full justify-between">
                 <Header title="Chat" />
--- app/index.jsx
+++ app/index.jsx
@@ -2,11 +2,16 @@ import { Button, ScrollView, Text, TouchableOpacity, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import CustomButton from '../components/CustomButton';
 import { router } from 'expo-router';
+import { LinearGradient } from 'expo-linear-gradient';
 
 
 export default function App() {
   return (
     <SafeAreaView className="bg-primary h-full">
+      <LinearGradient
+          colors={['#0E0C1D', '#07060E']}
+          className="absolute h-[100vh] left-0 right-0 top-0"
+      />
       <ScrollView contentContainerStyle={{ height: '100%'}}>
         <View className="w-full justify-center items-center h-full p-10">
           <Text className="text-white text-5xl font-bold text-center">
